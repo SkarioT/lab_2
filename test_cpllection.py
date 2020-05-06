5cd7aae84e7d2575f12aab236bbe6b23b90e9826
SkarioT <vlad.skariot@gmail.com>
2020-05-06 20:31:06 +0300

    test_coll_v2

--- test_cpllection.py
+++ test_cpllection.py
@@ -1,6 +1,19 @@
-a=input("Введите значение 1 : ")
-b=input("Введите значение 2 : ")
-C=input("Введите значение 3 : ")
-colection=(a,b,c)
+id=0
+metadata={}
+while int(id)<(int(id)+1):
+    id+=1
+    FIO=input("Введите ваше ФИО : ")
+    SEX=input("Введите ваш пол : ")     
+    AGE=input("Введите ваш возраст : ")
+    weight=input("Введите ваш Вес : ")
+    height=input("Введите ваш Рост : ")
+    metadata[id]=({"fio" : FIO},{"sex":SEX},{"age": AGE},{"weight",weight},{"height" : height})
+    if FIO == 'q':
+        break
 
-print(colection)
+i=0
+while int(i) < int(id):
+    i+=1
+    print(metadata.get(i))
+
+
