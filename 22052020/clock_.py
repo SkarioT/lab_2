fca866bf87b21a58386b1bb08707fe6f5a2fdf3a
SkarioT <vlad.skariot@gmail.com>
2020-05-21 01:06:22 +0300

    'pre-release'

--- 22052020/clock_.py
+++ 22052020/clock_.py
@@ -12,16 +12,19 @@ c6=['▓▓▓▓▓▓▓▓','▓▓▓▓▓▓▓▓','▓▓      ','▓▓
 c7=['▓▓▓▓▓▓▓▓','▓▓▓▓▓▓▓▓','▓▓    ▓▓','      ▓▓','      ▓▓','      ▓▓','      ▓▓','      ▓▓','      ▓▓','      ▓▓']
 c8=['▓▓▓▓▓▓▓▓','▓▓▓▓▓▓▓▓','▓▓    ▓▓','▓▓    ▓▓','▓▓▓▓▓▓▓▓','▓▓▓▓▓▓▓▓','▓▓    ▓▓','▓▓    ▓▓','▓▓▓▓▓▓▓▓','▓▓▓▓▓▓▓▓']
 c9=['▓▓▓▓▓▓▓▓','▓▓▓▓▓▓▓▓','▓▓    ▓▓','▓▓    ▓▓','▓▓▓▓▓▓▓▓','▓▓▓▓▓▓▓▓','      ▓▓','      ▓▓','▓▓▓▓▓▓▓▓','▓▓▓▓▓▓▓▓']
-d_c0=['        ','        ','   ▓▓   ','   ▓▓   ','        ','        ','   ▓▓   ','   ▓▓   ','        ','        ']
 
+d_c0=['        ','        ','   ▓▓   ','   ▓▓   ','        ','        ','   ▓▓   ','   ▓▓   ','        ','        ']
+# i=0
 c=[c0,c1,c2,c3,c4,c5,c6,c7,c8,c9]
 
-#c=[['■■■','■ ■','■ ■','■ ■','■■■'],['■■ ',' ■ ',' ■ ',' ■ ','■■■'],['■■■','  ■','■■■','■  ','■■■']]
-
-column1,column2,column3,column4,column5,column6=0,1,4,5,7,9
 while True:
     cdt=datetime.datetime.now()
-    str_cdt=datetime.datetime.strftime(cdt,'%d %m %Y %H %M %S').split()
+    str_cdt=datetime.datetime.strftime(cdt,'%H %M %S').split()
+    column1,column2,column3,column4,column5,column6=int(str_cdt[0][0]),int(str_cdt[0][1]),int(str_cdt[1][0]),int(str_cdt[1][1]),int(str_cdt[2][0]),int(str_cdt[2][1])
+    # sec_p=(0,6,12,18,24,30,36,42,48,54)
+    # sec=(int(str_cdt[2][1]))
+    # if sec in sec_p:
+        # i=sec
     print(c[column1][0],c[column2][0],d_c0[0],c[column3][0],c[column4][0],d_c0[0],c[column5][0],c[column6][0])
     print(c[column1][1],c[column2][1],d_c0[1],c[column3][1],c[column4][1],d_c0[1],c[column5][1],c[column6][1])
     print(c[column1][2],c[column2][2],d_c0[2],c[column3][2],c[column4][2],d_c0[2],c[column5][2],c[column6][2])
@@ -32,16 +35,6 @@ while True:
     print(c[column1][7],c[column2][7],d_c0[7],c[column3][7],c[column4][7],d_c0[7],c[column5][7],c[column6][7])
     print(c[column1][8],c[column2][8],d_c0[8],c[column3][8],c[column4][8],d_c0[8],c[column5][8],c[column6][8])
     print(c[column1][9],c[column2][9],d_c0[9],c[column3][9],c[column4][9],d_c0[9],c[column5][9],c[column6][9])
-
-
-
-
-
-
-
-
-
-
 
     time.sleep(1)
     os.system('cls')
